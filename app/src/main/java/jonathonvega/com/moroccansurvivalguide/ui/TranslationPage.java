c20412fdc97968a7b4ae1a17298b5c2dada9158e
Jonathon Vega <jfvega@live.com>
2016-11-16 17:31:10 -0500

    Initial Commit

---
+++ app/src/main/java/jonathonvega/com/moroccansurvivalguide/ui/TranslationPage.java
@@ -0,0 +1,35 @@
+package jonathonvega.com.moroccansurvivalguide.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import jonathonvega.com.moroccansurvivalguide.R;
+
+public class TranslationPage extends FragmentActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+    /*@Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.activity_phrase_details, container, false);
+        TextView tv = (TextView)v.findViewById(R.id.detailsEnglishText);
+        //tv.setText
+
+        return v;
+    }*/
+}
